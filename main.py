ca7e98b073945e01540c13bfadfbddae9c40aadb
Jetmate <antonoutkine@gmail.com>
2017-08-13 02:49:21 -0500

    no longer stupid

--- main.py
+++ main.py
@@ -102,14 +102,6 @@ def log():
         queue.remove(session['id'])
         cache.set('queue', queue)
 
-
-@app.after_request
-def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 ############
 
 if __name__ == '__main__':
